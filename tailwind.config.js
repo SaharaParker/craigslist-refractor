de80611937b7384402b7be15782c72cd96000a63
Sahara <THsahara@gmail.com>
2021-12-19 13:48:10 -0500

    testing extension of theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,23 @@
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
-  extend: {
-    colors: {
-      brown: {
-        50: '#fdf8f6',
-        100: '#f2e8e5',
-        200: '#eaddd7',
-        300: '#e0cec7',
-        400: '#d2bab0',
-        500: '#bfa094',
-        600: '#a18072',
-        700: '#977669',
-        800: '#846358',
-        900: '#43302b',
-      },
-    }
+  theme: {
+    extend: {
+      colors: {
+        brown: {
+          50: '#fdf8f6',
+          100: '#f2e8e5',
+          200: '#eaddd7',
+          300: '#e0cec7',
+          400: '#d2bab0',
+          500: '#bfa094',
+          600: '#a18072',
+          700: '#977669',
+          800: '#846358',
+          900: '#43302b',
+        },
+      }
+    },
   },
   variants: {
     extend: {},
